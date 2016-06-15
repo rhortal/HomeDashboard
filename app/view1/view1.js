4b970a832f7f5b77a536435d3caccd40a66bf7fc
Roberto Hortal <rhortal@gmail.com>
2016-06-15 21:54:26 +0100

    Got the cameras working
    Had to set up a proxy and battle the syntax but it works!

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,11 +6,10 @@ angular.module('myApp.view1', ['ngRoute'])
     controller: 'View1Ctrl'
   });
 }])
-.controller('View1Ctrl', [ '$scope', function($scope) {
-$scope.name = '';
-$scope.names = [{name:"Chris"}, {name:"Calvin"}];
-$scope.addName = function() {
-$scope.names.push( {'name':$scope.name} );
-$scope.name = '';
-};
+.controller('View1Ctrl', [ '$scope', '$log', function($scope) {
+$scope.cameras = [{name:"Study", video:"http://tiorob.duckdns.org/study/videostream.cgi?rate=0", image:"http://tiorob.duckdns.org/study/snapshot.cgi"},
+{name:"Garage", video:"http://tiorob.duckdns.org/garage/videostream.cgi?rate=0", image:"http://tiorob.duckdns.org/garage/snapshot.cgi"}];
+$scope.toggleVideo = function($event, image, video) {
+  $event.currentTarget.src === image ? $event.currentTarget.src = video : $event.currentTarget.src = image;
+  };
 }]);
